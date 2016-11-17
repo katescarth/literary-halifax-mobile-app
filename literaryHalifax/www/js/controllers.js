2edaf1fd42156d2c62e0e8606b60d9e1d5d0b2a6
David Walker <walk.dv@gmail.com>
2016-11-17 18:39:41 -0400

    begin adding new functions to server

--- literaryHalifax/www/js/controllers.js
+++ literaryHalifax/www/js/controllers.js
@@ -60,6 +60,14 @@ angular.module('literaryHalifax')
 
 }).controller('storiesCtrl', function($scope, $state, server){
 
+  MAP_STATE  =  'app.stories.map'
+  LIST_STATE = 'app.stories.list'
+  CLEAR_BUTTON= "button button-full button-outline button-balanced"
+  FILLED_BUTTON= "button button-full button-balanced"
+
+  $scope.mapButton = FILLED_BUTTON
+  $scope.listButton=CLEAR_BUTTON
+
   $scope.mapInfo = {
     center:"44.6474,-63.5806",
     zoom: 15
@@ -73,15 +81,26 @@ angular.module('literaryHalifax')
     console.log(error)
   })
 
+
   $scope.toMapTab = function(){
-    $state.go('app.stories.map')
+    $state.go(MAP_STATE)
   }
 
   $scope.toListTab = function(){
-    $state.go('app.stories.list')
+    $state.go(LIST_STATE)
   }
 
-
+  $scope.$root.$on('$stateChangeSuccess',
+    function(event, toState, toParams, fromState, fromParams){
+      if (toState.name==MAP_STATE) {
+        $scope.mapButton = FILLED_BUTTON
+        $scope.listButton=CLEAR_BUTTON
+      } else if (toState.name=LIST_STATE) {
+        $scope.mapButton = CLEAR_BUTTON
+        $scope.listButton=FILLED_BUTTON
+      }
+    }
+  )
 
 
 
--- literaryHalifax/www/js/server.js
+++ literaryHalifax/www/js/server.js
@@ -1,28 +1,66 @@
 angular.module('literaryHalifax')
+
+/*
+ * Right now, this is all fixture data. The purpose of this code is to separate
+ * the rest of the app from the server (this is the only part of the code that
+ * knows there's not actually a server)
+
+ * Spec for place:
+ *  id: a unique identifier (str)
+ *  name: The name of the place (str)
+ *  location: the lat,lng of the place (str) TODO update to numerical data
+ *  description: text description of the place (str)
+ *  images: a list of images associated with the place. The first image is the
+            thumbnail/main image (array[image])
+ *  audio:  a list of audio tracks associated with the place. array[audio]
+
+ * getPlaces(): resolves to a list of places. The places have ids, locations,
+                and names - enough  info to display in a list without later
+                requests.
+ * placeInfo(id, attributes): resolves to an object identical to attributes
+                              but with properties identical to those of the
+                              place with the given id. 
+ *
+ */
 .factory('server', function($timeout){
   var SMALL_DELAY = 200
   var LARGE_DELAY = 2000
+
+  var places = [
+    {
+      name:"Halifax Central Library",
+      location:"44.6431,-63.5752",
+      id: "place-id-1"
+    },
+    {
+      name:"Public Gardens",
+      location:"44.6428,-63.5821",
+      id: "place-id-2"
+    },
+    {
+      name:"The Dingle",
+      location:"44.6304,-63.6028",
+      id: "place-id-3"
+    }
+  ]
+
   server = {
     getPlaces:function(){
+
+      result = []
+
+      for(i=0;i<places.length;i++){
+        place = places[i]
+        result.push({
+          name:place.name,
+          location:place.location,
+          id:place.id
+        })
+      }
+
       return $timeout(function(){
-        return [
-          {
-            name:"Halifax Central Library",
-            location:"44.6431,-63.5752",
-            id: "place-id-1"
-          },
-          {
-            name:"Public Gardens",
-            location:"44.6428,-63.5821",
-            id: "place-id-2"
-          },
-          {
-            name:"The Dingle",
-            location:"44.6304,-63.6028",
-            id: "place-id-3"
-          }
-        ]
-      },SMALL_DELAY)
+        return result
+      }, SMALL_DELAY)
 
     }
   }
