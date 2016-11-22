e45c9f31c2ee184d81017f9e2e094e67fd8fb3f4
David Walker <walk.dv@gmail.com>
2016-11-22 17:21:58 -0400

    move 'card' display out of the overscrollPane directive: the programmer have control of how the overlay looks

--- literaryHalifax/www/components/overscrollPane/overscrollPane.js
+++ literaryHalifax/www/components/overscrollPane/overscrollPane.js
@@ -1,10 +1,11 @@
+/*
+* This component displays the transcluded contents overlayed on whatever
+* is behind the component. The contents are displayed starting partway down
+* the screen, but can be scrolled all the way up. The scrolling does not affect
+* the content which is overlayed.
+* DO NOT TRANSCLUDE AN <ION-CONTENT> BLOCK
+*/
 angular.module('literaryHalifax')
-//  This component displays the transcluded contents overlayed on whatever
-//  is behind the component. The contents are displayed on a  'card' which
-//  starts partway down the screen, but can be scrolled all the way up. The
-//  scrolling does not affect the content which is overlayed.
-
-//  DO NOT TRANSCLUDE AN <ION-CONTENT> BLOCK
 .directive('overscrollPane', function(){
   return {
     transclude: true,
