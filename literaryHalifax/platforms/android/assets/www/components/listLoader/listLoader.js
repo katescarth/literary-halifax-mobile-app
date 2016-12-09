85a7f0e1b9074eb1a90c22611f289e28069583f5
David Owen Walker <walk.dv@gmail.com>
2016-12-09 15:47:11 -0400

    remove android build from git

--- literaryHalifax/platforms/android/assets/www/components/listLoader/listLoader.js
+++
@@ -1,9 +0,0 @@
-angular.module('literaryHalifax').directive('listLoader', function () {
-    return {
-        scope: {
-            loadingMsg: '=',
-            errorMsg: '='
-        }
-        , templateUrl: 'components/listLoader/listLoader.html'
-    }
-})
