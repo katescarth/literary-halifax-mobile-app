3c2d87fdeeff1c51d63bb6e139bc2f32dadc6d0b
David Owen Walker <walk.dv@gmail.com>
2016-12-23 11:31:56 -0400

    documentation, organization

--- literaryHalifax/www/js/utilities.js
+++ literaryHalifax/www/js/utilities.js
@@ -3,11 +3,15 @@ angular.module('literaryHalifax')
 /*
  * This file is a dumping ground for reused code.
  */
-.factory('utils', function($q){
+.factory('utils', function($q, $ionicPlatform){
     
     
     
     var utils={
+        // returns the distance between two lat/lng objects, in kilometers.
+        // To perform this calculation quickly, we treat the earth as flat.
+        // 111.1km/lat, 79.3km/lng is specific to Halifax, so the distances 
+        // will be inaccurate from far away
         distance:function(from,to){
             if(!(from.lat&&from.lng&&to.lat&&to.lng)){
                 console.log('bad lat/lng format')
@@ -18,24 +22,31 @@ angular.module('literaryHalifax')
             squarekms = Math.pow((dLat*111.1),2) + Math.pow((dLng*79.3),2)
             return Math.sqrt(squarekms)
         },
+        
+        // returns a promise which resolves to a lat/lng object
+        // the error format is not well defined
         getPosition:function(options){
             var deferred = $q.defer()
-            if(navigator.geolocation){
-                navigator.geolocation.getCurrentPosition(
-                    function(currentPosition){
-                        deferred.resolve({
-                            lat: currentPosition.coords.latitude,
-                            lng:currentPosition.coords.longitude
-                        })
-                    },
-                    function(error){
-                        deferred.reject(error)
-                    },
-                    options
-                )
-            } else {
-                deferred.reject(error)                
-            }
+            
+            $ionicPlatform.ready(function(){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(
+                        function(currentPosition){
+                            deferred.resolve({
+                                lat: currentPosition.coords.latitude,
+                                lng:currentPosition.coords.longitude
+                            })
+                        },
+                        function(error){
+                            deferred.reject(error)
+                        },
+                        options
+                    )
+                } else {
+                    deferred.reject("No navigator!")
+                }
+            })
+            
             return deferred.promise
         }
     }
