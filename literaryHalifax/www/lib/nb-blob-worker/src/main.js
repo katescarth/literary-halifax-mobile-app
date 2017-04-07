157dfa956910f8233f9543b013887da5440c072d
David-Owen-Walker <walk.dv@gmail.com>
2017-04-07 17:40:13 -0300

    remove experimental libraries

--- literaryHalifax/www/lib/nb-blob-worker/src/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/**
- * main interface to import then export
- */
-
- export {default as NbBlobWorker} from './nb-blob-worker.js';
- export {NbBlobWorkerEvent} from './nb-blob-worker-event.js';
